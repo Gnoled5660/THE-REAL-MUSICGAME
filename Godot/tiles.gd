12148cb1ceee2951f0c638df470d40a8bbd50966
Jason Liu <injadude01@gmail.com>
2026-07-05 14:53:54 +0800

    fdg

--- Godot/tiles.gd
+++ Godot/tiles.gd
@@ -1,36 +1,51 @@
-extends Node2D
-@onready var body: RigidBody2D = $RigidBody2D
-@onready var animated_sprite: AnimatedSprite2D = $RigidBody2D/AnimatedSprite2D
-var textNum = 0
-var points1 = 0;
-var points2 = 0;
-var animation = ""
+extends Area2D # Changed to Area2D for easier movement control
+
+signal nextTile
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var textNum: int = 0
+var animation: String = ""
 
 func _ready() -> void:
 	genRandTexture()
-	animated_sprite.play(animation)
 
-func genRandTexture():
+func genRandTexture() -> void:
 	textNum = randi_range(1, 4)
 	animation = str(textNum)
+	animated_sprite.play(animation)
 
-func _process(_delta: float) -> void: 
-	if checkInput():
-		Globals.points1 += 5
-		Globals.increaseSpeed()
-		print("Soup Score: " + str(Globals.points1))
-		queue_free()
-		nextTile.emit()
-	else:
-		queue_free()
-		nextTile.emit()
-		
-func checkInput(): 
-	if Input.is_action_just_pressed("down")&&textNum==1:
+func _process(delta: float) -> void:
+	# Make the arrow actually fall down the screen!
+	position.y += Global.startSpeed * delta
+	
+	# If any relevant key is pressed, check if it matches this arrow
+	if Input.is_action_just_pressed("down") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right") or Input.is_action_just_pressed("up"):
+		if checkInput():
+			Global.points1 += 5
+			Global.increaseSpeed()
+			print("Correct! Soup Score: " + str(Global.points1))
+			destroy_and_next()
+		else:
+			print("Wrong key!")
+			# Optional: add penalty to points2 here if Bernard is the opponent/bad guy
+
+	# If the arrow falls off the bottom of the screen (e.g., Y > 700), miss!
+	if position.y > 700:
+		print("Missed!")
+		destroy_and_next()
+
+func checkInput() -> bool:
+	if Input.is_action_just_pressed("down") and textNum == 1:
 		return true
-	if Input.is_action_just_pressed("left")&&textNum==2:
+	if Input.is_action_just_pressed("left") and textNum == 2:
 		return true
-	if Input.is_action_just_pressed("right")&&textNum==3:
+	if Input.is_action_just_pressed("right") and textNum == 3:
 		return true
-	if Input.is_action_just_pressed("up")&&textNum==4:
-		return true	
+	if Input.is_action_just_pressed("up") and textNum == 4:
+		return true
+	return false
+
+func destroy_and_next() -> void:
+	nextTile.emit()
+	queue_free()
